0487469bfd6534e9a176788d2f8c7c1941dad79f
YupenBob <yupenbob@163.com>
2022-08-03 15:09:51 +0800

    Site updated: 2022-08-03 15:09:49

--- js/FunnyTitle.js
+++ js/FunnyTitle.js
@@ -2,11 +2,11 @@
  var titleTime;
  document.addEventListener('visibilitychange', function () {
      if (document.hidden) {
-         document.title = 'ヽ(●-`Д´-)ノ你丑你就走！';
+         document.title = 'o((>ω< ))o别走啊！';
          clearTimeout(titleTime);
      }
      else {
-         document.title = 'ヾ(Ő∀Ő3)ノ你帅就回来！' + OriginTitle;
+         document.title = 'ヾ(Ő∀Ő3)ノ欢迎回来！' + OriginTitle;
          titleTime = setTimeout(function () {
              document.title = OriginTitle;
          }, 2000);
